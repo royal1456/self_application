ba8a9a8a43906d2b9a72d0ababee19fc8ed85e5f
royal1456 <36342283+royal1456@users.noreply.github.com>
2019-03-03 17:25:41 +0530

    Update preview selector.py
    added details

--- preview selector.py
+++ preview selector.py
@@ -1,12 +1,13 @@
+#a simple program to copy high resolution as per  low ones in a directory (uses file number to match them)
 import os
 import shutil
 def copy(add,org):
     new=org+"\\"+add
     return(new)
 
-loc=input('preview ki location:')
-org=input('original ki location:')
-fol=input('save krne vale folder ki location:')
+loc=input('preview ki location:')#takes location of preview(low resoslution files)
+org=input('original ki location:')#takes location of original(high resoslution files)
+fol=input('save krne vale folder ki location:')#takes location of path or folder to saved
 
 size=len(os.listdir(loc))
 pos = len(os.listdir(org))
