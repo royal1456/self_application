64110a93e68b01de981e19bb0bfa25f0afc376f7
HIMANSHU <36342283+royal1456@users.noreply.github.com>
2021-03-14 22:33:22 +0530

    Update preview selector.py

--- preview selector.py
+++ preview selector.py
@@ -1,28 +1,49 @@
-#a simple program to copy high resolution as per  low ones in a directory (uses file number to match them)
+#a simple program to copy duplicates/high resolution as per  original/low ones in a directory (uses file number to match them)
 import os
 import shutil
-def copy(add,org):
-    new=org+"\\"+add
-    return(new)
+flag=0
+log=[]
+loc=input('preview  location:') #takes location of preview(low resoslution files)
+org=input('original  location:') #takes location of original(high resoslution files)
+fol=input('save folder location:') #takes location of path or folder to saved
 
-loc=input('preview ki location:')#takes location of preview(low resoslution files)
-org=input('original ki location:')#takes location of original(high resoslution files)
-fol=input('save krne vale folder ki location:')#takes location of path or folder to saved
+def write(loc):
+    try:
+        file =open(os.path.join(loc,"uncopied.txt"),'w')
+        file.write("\n".join(log))
+        file.close()
+        print("Written")
+    except:
+        print("Unable to write try giving another path to write text file \n or write exit to exit")
+        new_loc=input()
+        if(new_loc!="exit"):
+            write(new_loc)
 
-size=len(os.listdir(loc))
-pos = len(os.listdir(org))
 orgl=os.listdir(org)
 locl = os.listdir(loc)
 foll = os.listdir(fol)
-os.chdir(fol)
-for i in range(size):
-   for j in range(pos):
-        if (locl[i]==orgl[j]):
-             new=copy(locl[i],org)
-
-             shutil.copy(new, fol)
-             break
-
-
-p=input()
-
+for i in locl:
+    try:
+        new=os.path.join(org,i)
+        shutil.copy(new, fol)
+    except:
+        flag=1
+        log.append(i)
+        
+if(flag==0):
+    print("ALL Preview files read and copied  prss an key to exit")
+    p=input()
+else:
+    print("All files are not copied\n",*log,sep=",")
+    print("Write copyto write their names in file in save  folder  or type exit")
+    while(1):
+        inp=input()
+        if(inp=="copy"):
+            write(fol)
+            print("press any key to exit")
+            input()
+            break
+        elif(inp=="exit"):
+            break
+        print("make sure your spelling are correct copy,no caps")
+        
